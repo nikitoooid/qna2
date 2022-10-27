87f3f351d3ea8d94c84be0c184dbf58196b89cfd
nikitoooid <nekytnp@gmail.com>
2022-10-27 13:20:21 +0300

    changed models specs, added spec helpers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:answers).dependent(:destroy) }
+  it { should have_many(:questions).dependent(:destroy) }
+
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :password }
+
+  describe '#author_of?' do
+    let(:user) { create(:user) }
+    let(:another_user) { create(:user) }
+    let(:object) { create(:question, user: user) }
+
+    it 'returns true if user is creator of the object' do
+      expect(user).to be_author_of(object)
+    end
+
+    it 'returns false if user is not creator of the object' do
+      expect(another_user).to_not be_author_of(object)
+    end
+  end
 end
