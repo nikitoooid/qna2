96b7579cd401f909136caac5c7a137c2acfb9711
nikitoooid <nekytnp@gmail.com>
2022-11-10 13:58:03 +0200

    Only author can edit his answer

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -5,8 +5,8 @@ feature 'User can edit his answer', %q{
   As an author of the answer
   I'd like to be able to edit my answer
 } do
-  given!(:user) { create(:user) }
-  given!(:question) { create(:question, user: user) }
+  given(:user) { create(:user) }
+  given(:question) { create(:question, user: user) }
   given!(:answer) { create(:answer, question: question, user: user) }
 
   scenario "Unauthenticated user can't edit answer" do
@@ -16,13 +16,17 @@ feature 'User can edit his answer', %q{
   end
 
   describe 'Authenticated user', js: true do
+
+    given(:another_user) { create(:user) }
+    given!(:another_answer) { create(:answer, question: question, user: another_user) }
+
     background do
       sign_in(user)
       visit question_path(question)
     end
 
     scenario 'edit his answer with valid attributes' do
-      within '.answers' do
+      within (".answers .answer[data-id='#{answer.id}']") do
         click_on 'Edit'
         fill_in 'Body', with: 'edited answer'
         click_on 'Save'
@@ -34,11 +38,20 @@ feature 'User can edit his answer', %q{
     end
 
     scenario 'edit his answer with errors' do
+      within (".answers .answer[data-id='#{answer.id}']") do
+        click_on 'Edit'
+        fill_in 'Body', with: nil
+        click_on 'Save'
 
+        expect(page).to have_content answer.body
+        expect(page).to have_selector 'textarea'
+      end
     end
 
     scenario "tries to edit someone else's answer" do
-
+      within (".answers .answer[data-id='#{another_answer.id}']") do
+        expect(page).to_not have_link 'Edit'
+      end
     end
   end
 end
