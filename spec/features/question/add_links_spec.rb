b639e7637460e4e323e0c1afb44e8d4c5795c830
nikitoooid <nekytnp@gmail.com>
2022-11-19 14:03:32 +0200

    User can add links when editing the question

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -6,32 +6,66 @@ describe 'User can add links to question', "
   I'd like to be able to add links
 " do
   let(:user) { create(:user) }
+  let(:question) { create(:question, user: user) }
   let(:gist_url) { 'https://gist.github.com/nikitoooid/07ebe134f166003ac20825e5a291c5eb' }
 
   before do
     sign_in(user)
-    visit new_question_path
-
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'test question body text'
   end
 
-  it 'User adds link when asks question' do
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: gist_url
+  describe 'When user creating the question' do
+    before do
+      visit new_question_path
+  
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'test question body text'
+    end
+  
+    it 'user adds link' do
+      fill_in 'Link name', with: 'My gist'
+      fill_in 'Url', with: gist_url
+  
+      click_on 'Ask'
+  
+      expect(page).to have_link 'My gist', href: gist_url
+    end
 
-    click_on 'Ask'
-
-    expect(page).to have_link 'My gist', href: gist_url
+    it 'user tries to add link with invalid params' do
+      fill_in 'Link name', with: 'My gist'
+      fill_in 'Url', with: 'not valid url'
+  
+      click_on 'Ask'
+  
+      expect(page).not_to have_link 'My gist'
+      expect(page).to have_content 'Url is not valid'
+    end
   end
+  
+  describe 'When user edits the question', js: true do
+    before do
+      visit question_path(question)
 
-  it 'User tries to add link with invalid params' do
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: 'not valid url'
+      click_on 'Edit'
+      click_on 'add link'
+    end
+    
+    it 'User adds link' do
+      fill_in 'Link name', with: 'My gist'
+      fill_in 'Url', with: gist_url
+  
+      click_on 'Save'
+  
+      expect(page).to have_link 'My gist', href: gist_url
+    end
 
-    click_on 'Ask'
-
-    expect(page).not_to have_link 'My gist'
-    expect(page).to have_content 'Url is not valid'
+    it 'User tries to add link with invalid params' do
+      fill_in 'Link name', with: 'My gist'
+      fill_in 'Url', with: 'not valid url'
+  
+      click_on 'Save'
+  
+      expect(page).not_to have_link 'My gist'
+      expect(page).to have_content 'Url is not valid'
+    end
   end
 end
